cbcf57278779757d4a264c7b9956d4ff68245d39
Messias Junior <messias.junior@getchip.uk>
2024-10-10 18:04:37 +0100

    generate java classes

--- .github/workflows/scripts/generate-kotlin-classes.sh
+++ .github/workflows/scripts/generate-kotlin-classes.sh
@@ -1,10 +1,10 @@
-LANGS="kotlin"
+LANGS="java:beans"
 
 for LANG in $LANGS
 do
     for FILE in thrift/layout-ras/thrift/*.thrift
     do
-        thrift --gen $LANG $FILE
+        thrift --gen $LANG $FILE -out esusab-java
     done
 
 done
