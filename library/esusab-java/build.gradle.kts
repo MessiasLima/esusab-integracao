6deb6dcb677dac8c2af9299bcfa252a94c26958c
Messias Junior <messias.junior@getchip.uk>
2024-10-14 11:22:15 +0100

    fix auth

--- library/esusab-java/build.gradle.kts
+++ library/esusab-java/build.gradle.kts
@@ -15,9 +15,9 @@ repositories {
 dependencies {
     // libthrift is already on 0.21.0, however the files were compiled using 0.9.3 version
     implementation("org.apache.thrift:libthrift:0.9.3")
+
+    // javax.annotation-api:1.3.2 is dependency of libthrift:0.9.3. We must update it as soon as possible
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 val repo: String = System.getenv("GITHUB_REPOSITORY") ?: "laboratoriobridge/esusab-integracao"
